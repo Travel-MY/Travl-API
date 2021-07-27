7a2d62804adc78a8f2043ae02d243c5fa80461d8
Ikmal Azman <opikmalazman@gmail.com>
2021-07-27 22:36:11 +0800

    setup server and heroku deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+const middleWare = jsonServer.defaults()
+const port = process.env.PORT
+
+server.use(middleWare)
+server.use(router)
+
+server.listen(port)
